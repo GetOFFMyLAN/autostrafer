69c49ebaf4e1adfca9bca4a84a3c98b6525a74e2
John <axel.ramone@gmail.com>
2019-05-26 16:20:18 -0500

    replaced: titlechanger.js with modifiers.js

---
+++ JS/modifiers.js
@@ -0,0 +1,49 @@
+function displayTitle() {
+	var index = 0;
+	var titleArr = [
+		"a",
+		"au",
+		"aut",
+		"auto",
+		"autos",
+		"autost",
+		"autostr",
+		"autostra",
+		"autostraf",
+		"autostrafe",
+		"autostrafer",
+		"autostrafer.",
+		"autostrafer.c",
+		"autostrafer.cc"
+	];
+
+	function changeTitle() {
+		document.title = titleArr[index];
+		index++;
+		if (index == titleArr.length) {
+			index = 0;
+		}
+	};
+
+	window.setInterval(changeTitle, 500);
+}
+
+function displayHeader() {
+	var header = document.getElementById("header");
+	var index = 0;
+	var headerText = ["autostrafer|", "autostrafer"];
+
+	function lines() {
+		header.innerHTML = headerText[index];
+		index++;
+
+		if (index == headerText.length) {
+			index = 0;
+		}
+	};
+	
+	window.setInterval(lines, 600);
+}
+
+displayTitle();
+displayHeader();
